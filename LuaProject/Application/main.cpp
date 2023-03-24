a7d4a85bf57f8e004f49e1b09d2df46c051866d9
klaand01 <kmda23@hotmail.com>
2023-03-24 09:56:30 +0100

    Added dumpstack

--- LuaProject/Application/main.cpp
+++ LuaProject/Application/main.cpp
@@ -49,59 +49,33 @@ void luaThreadLoop(lua_State* L)
 	}
 }
 
+std::string GetValueString(lua_State* L, int i)
+{
+	switch (lua_type(L, i))
+	{
+	case LUA_TNIL: return "nil";
+	case LUA_TBOOLEAN:
+		return lua_toboolean(L, i) ? "true" : "false";
+	case LUA_TNUMBER: return std::to_string(lua_tonumber(L, i));
+	case LUA_TSTRING: return lua_tostring(L, i);
+	default: return "";
+	}
+}
+
 void dumpStack(lua_State* L)
 {
 	int size = lua_gettop(L);
-	std::cout << " " << std::string(11, '-');
-	std::cout << " STACK BEGIN ";
-	std::cout << " " << std::string(11, '-');
 
+	std::cout << "--- STACK BEGIN ---" << std::endl;
 	for (int i = size; i > 0; i--)
 	{
-		int type = lua_type(L, i);
-		std::string typeName = lua_typename(L, type);
-		std::string value;
-
-		if (type == LUA_TSTRING)
-		{
-			value = "\"";
-			value += lua_tostring(L, i);
-
-			if (value.size() > 11)
-			{
-				value.resize(9);
-				value += " . . ";
-			}
-
-			value += "\"";
-		}
-		else if (type == LUA_TBOOLEAN)
-		{
-			value = lua_toboolean(L, i) ? "true" : "false";
-		}
-		else if (type == LUA_TNIL)
-		{
-			value = "nil";
-		}
-		else if (lua_isnumber(L, i))
-		{
-			value = std::to_string(lua_tonumber(L, i));
-		}
-		else if (lua_isstring(L, i))
-		{
-			value = lua_tostring(L, i);
-		}
-
-		std::cout << std::setw(3) << i << " | ";
-		std::cout << typeName << std::setw(25 - typeName.size()) << value;
-		std::cout << std::setw(5 - typeName.size() - value.size()) << " | ";
-		std::cout << std::setw(2) << -(size - i + 1);
-		std::cout << std::endl;
+		std::cout << i
+			<< "\t"
+			<< lua_typename(L, lua_type(L, i))
+			<< "\t\t" << GetValueString(L, i)
+			<< std::endl;
 	}
-
-	std::cout << " " << std::string(12, '-');
-	std::cout << " STACK END ";
-	std::cout << std::string(12, '-') << std::endl;
+	std::cout << "---- STACK END ----" << std::endl;
 }
 
 int main()
